bd159c2a44f6254990744ce1219ccb454b775fa4
NCoryDev <nick.james.cory@gmail.com>
2020-05-09 23:35:00 +0100

    including script

---
+++ script.js
@@ -0,0 +1,11 @@
+var x = document.getElementById('demo');
+function myFunction(){
+
+x.innerHTML = "Changed";
+
+}
+
+var y = document.getElementById("demo");
+function resetButton(){
+  y.innerHTML = "Original";
+}
